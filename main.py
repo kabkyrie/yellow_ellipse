a75368ef8679dfcf7877efa497d48c58bd46e1c4
Nikita <kabanovnikita54@gmail.com>
2024-12-02 21:58:21 +1000

    I like it :)

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import sys
-import random
-from PyQt6.QtCore import Qt, QPoint
+
+from random import randint
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
 
 
-class YellowEllipse(QWidget):
+class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(400, 400)
 
-        self.label = QLabel(self)
-        self.label.move(0, 0)
-        self.label.resize(400, 350)
+        self.setGeometry(600, 600, 600, 600)
+        self.move(100, 100)
+        self.setWindowTitle('Рисование')
 
-        self.button = QPushButton(self)
-        self.button.move(150, 350)
+        self.button = QPushButton('Рисовать', self)
+        self.button.move(250, 550)
         self.button.resize(100, 30)
-        self.button.setText("Рисование")
-        self.button.clicked.connect(self.drawing)
+        self.do_paint = False
+        self.button.clicked.connect(self.paint)
 
-    def drawing(self):
-        qp = QPainter(self)
-        qp.drawEllipse(10, 10)
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawing(qp)
+            qp.end()
+        self.do_paint = False
 
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def drawing(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+
+        a_0, b_0, a_1, b_1, a_2, b_2 = randint(10, 500), randint(10, 500), randint(10, 500), \
+            randint(10, 500), randint(10, 500), randint(10, 500)
+        qp.drawEllipse(a_0, b_0, a_0, b_0)
+        qp.drawEllipse(a_1, b_1, a_1, b_1)
+        qp.drawEllipse(a_2, b_2, a_2, b_2)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = YellowEllipse()
-    window.show()
+    ex = Example()
+    ex.show()
     sys.exit(app.exec())
